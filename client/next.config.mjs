b7661ca19093ca831df80a2e77beecde13c1a34b
Tayyab <tayyabkhalid2004@gmail.com>
2026-04-19 12:53:51 +0500

    added neon .env stuff

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   /* config options here */
+  experimental: {
+    turbopack: {
+      root: path.join(process.cwd(), '../'),
+    },
+  },
 };
 
 export default nextConfig;
