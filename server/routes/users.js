84bbff19a28d60721e6c42ab46a5ba4f21848697
Muhammad Saad <msaad.95a@gmail.com>
2026-05-04 17:28:19 +0500

    Updated the Right Rail on Pulse to use WebSocket for dynamic loading and Fixed issue of profile picture API call limit set to 100KB

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { pool } = require('../config/dbConfig');
 const { authenticateToken } = require('../middleware/authMiddleware');
+const { isOnline } = require('../ws/presence');
 
 const router = express.Router();
 
@@ -285,7 +286,7 @@ router.delete('/:userId/follow', authenticateToken, async (req, res) => {
     }
 });
 
-const { isOnline } = require('../ws/presence');
+
 
 // GET /api/users/:userId/followers
 router.get('/:userId/followers', authenticateToken, async (req, res) => {
